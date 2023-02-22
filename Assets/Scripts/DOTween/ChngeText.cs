25d51375344cfaf907cea0eb2685375243881d97
Heruwim <heruwim25@gmail.com>
2023-02-22 12:51:18 +0200

    Text changing

---
+++ Assets/Scripts/DOTween/ChngeText.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChngeText : MonoBehaviour
+{
+    [SerializeField] private Text _text1;
+    [SerializeField] private Text _text2;
+    [SerializeField] private Text _text3;
+
+    private void Start()
+    {
+        _text1.DOText("Я заменил этот текст", 3);
+        _text2.DOText(" Это дополнение к тексту", 3).SetRelative();
+        _text3.DOText("Я взломал этот текст", 3, true, ScrambleMode.All);
+        _text3.DOColor(Color.red, 3);
+    }
+}
