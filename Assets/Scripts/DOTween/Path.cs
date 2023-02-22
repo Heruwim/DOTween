ff573bbaf0900093b23e94167ccb4655491e0c26
Heruwim <heruwim25@gmail.com>
2023-02-22 15:28:33 +0200

    Following a closed path

---
+++ Assets/Scripts/DOTween/Path.cs
@@ -0,0 +1,13 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField] private Vector3[] _wayPoints;
+    private void Start()
+    {
+        Tween tween = transform.DOPath(_wayPoints, 5, PathType.CatmullRom).SetOptions(true).SetLookAt(0.01f);
+
+        tween.SetEase(Ease.Linear).SetLoops(-1);
+    }
+}
