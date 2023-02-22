773a072ffd514aba31bc0350e6efdf519a7a85d1
Heruwim <heruwim25@gmail.com>
2023-02-22 11:32:31 +0200

    Repetition and looping

--- Assets/Scripts/DOTween/MoveTo.cs
+++ Assets/Scripts/DOTween/MoveTo.cs
@@ -6,7 +6,7 @@ public class MoveTo : MonoBehaviour
 {
     private void Start()
     {
-        transform.DOMove(new Vector3(0, 5, 0), 3).SetDelay(2);
+        transform.DOMove(new Vector3(0, 5, 0), 3).SetLoops(-1, LoopType.Yoyo);
     }
 
     
